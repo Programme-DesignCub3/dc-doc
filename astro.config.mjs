1854795cdea7ae53e30d6297b10fb8cb13627519
Ghassan <fsghassan2429d@gmail.com>
2024-11-13 22:29:05 +0700

    setup react integration and ts dynamic route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx()
-  ]
+  integrations: [tailwind(), mdx(), react(), sitemap()]
 });
