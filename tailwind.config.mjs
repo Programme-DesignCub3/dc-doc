3b2fde76a3cedc0ee003757950db6d1ee05dd7fc
Ghassan Fs <fsghassan2429d@gmail.com>
2024-11-13 16:22:28 +0700

    initial templating using bolt

---
+++ tailwind.config.mjs
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'rgb(94, 114, 228)',
+          light: 'rgb(134, 168, 255)',
+          dark: 'rgb(23, 37, 84)'
+        }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            color: 'rgb(203 213 225)',
+            h1: {
+              color: 'white',
+            },
+            h2: {
+              color: 'white',
+            },
+            h3: {
+              color: 'white',
+            },
+            h4: {
+              color: 'white',
+            },
+            strong: {
+              color: 'white',
+            },
+            code: {
+              color: 'rgb(134, 168, 255)',
+            },
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+            a: {
+              color: 'rgb(134, 168, 255)',
+              '&:hover': {
+                color: 'rgb(94, 114, 228)',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
