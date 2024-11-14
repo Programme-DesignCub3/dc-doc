c33332acd18058302f24d70835b577eb86e9b2f7
Ghassan Fs <fsghassan2429d@gmail.com>
2024-11-14 15:50:05 +0700

    add action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind";
 
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), sitemap()]
+  site: "https://Programme-DesignCub3.github.io",
+  base: "dc-doc",
+  build: {
+    format: "file",
+  },
+  integrations: [tailwind(), mdx(), react(), sitemap(), pagefind()],
 });
